c7258f74fdb307f635172a49881c8aa5d1a62f42
Radamanthus Batnag <rad@batnag.org>
2012-03-13 22:34:06 +0800

    Initial commit: working Rails 3, Ruby 1.9.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OutsourceYourFriends::Application.config.secret_token = '08271cebc1079b47032f3f340a5f1c45b617a0334b6d008f4a8afdeccf51aafab8ca69a74d19c53632513a64279603cc1b04e5f6eab3f197dba4b158b6f1066f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OutsourceYourFriends::Application.config.session_store :cookie_store, key: '_outsource_your_friends_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OutsourceYourFriends::Application.config.session_store :active_record_store
